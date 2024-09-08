3e207d99f5e4de29b6a4443f6916ad84bb1a15fe
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-09 08:56:37 +0900

    init: svgr 초기 세팅 및 사용 예시 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    // SVG imports를 처리하는 기존 규칙 가져오기
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      // svg imports 중 ?url로 끝나는 것에 대해서만 기존 규칙을 재적용
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // *.svg imports를 React 컴포넌트로 변환
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    // 처리가 완료된 *.svg를 무시하도록 파일 로더 규칙을 수정
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Alarm from '@/assets/svgs/icn_alarm.svg';
+import AlarmUrl from '@/assets/svgs/icn_alarm.svg?url';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className="text-purple">AlgoHub</div>;
+  return (
+    <div className="text-purple">
+      <Alarm width={50} height={50} fill='red' />
+			<Image width={50} height={50} src={AlarmUrl} alt='alarm' />
+      <img width={50} height={50} src={AlarmUrl.src} alt="alarm" />
+      <div
+        style={{
+          width: 50,
+          height: 50,
+          backgroundImage: `url(${AlarmUrl.src})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+    </div>
+  );
 }
