e97cd8bc47d4fcc4a538425c8cb8d538e1a46b57
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-10 01:21:30 +0900

    fix: 코리반영

--- generateIconExport.js
+++ generateIconExport.js
@@ -1,23 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // 파스칼 케이스 변환 함수
 function toPascalCase(fileName) {
-  return fileName.replace(/(^\w|_\w)/g, (match) => match.replace('_', '').toUpperCase());
+  return fileName.replace(/(^\w|_\w)/g, (match) =>
+    match.replace("_", "").toUpperCase()
+  );
 }
 
 // index.ts 파일 생성 함수
 function generateIndex() {
-  const svgDir = path.join(__dirname, 'src', 'assets', 'svgs'); // SVG 파일들이 있는 디렉토리 경로
-  const outputFile = path.join(svgDir, 'index.ts'); // 생성할 index.ts 경로
+  const svgDir = path.join(__dirname, "src", "assets", "svgs"); // SVG 파일들이 있는 디렉토리 경로
+  const outputFile = path.join(svgDir, "index.ts"); // 생성할 index.ts 경로
 
   // SVG 디렉토리에서 파일 목록 가져오기
   const files = fs
     .readdirSync(svgDir)
-    .filter((file) => file.endsWith('.svg')) // .svg 파일만 필터링
+    .filter((file) => file.endsWith(".svg")) // .svg 파일만 필터링
     .map((file) => ({
       fileName: file,
-      pascalName: toPascalCase(path.basename(file, '.svg')), // 확장자 제거 후 파스칼 케이스로 변환
+      pascalName: toPascalCase(path.basename(file, ".svg")), // 확장자 제거 후 파스칼 케이스로 변환
     }));
 
   // index.ts 파일 내용 작성
@@ -25,12 +27,12 @@ function generateIndex() {
     .map(({ fileName, pascalName }) => {
       return `export { default as ${pascalName} } from "./${fileName}";`;
     })
-    .join('\n');
+    .join("\n");
 
   // index.ts 파일 덮어쓰기
   fs.writeFileSync(outputFile, exportStatements);
 
-  console.log(`index.ts generated with ${files.length} exports.`);
+  // console.log(`index.ts generated with ${files.length} exports.`);
 }
 
 generateIndex();
