1644ae6ace04ca07cd70e8680cb2d653a0ec68f9
최주용 <121484561+wuzoo@users.noreply.github.com>
2024-09-10 20:47:42 +0900

    [ Docs ] Lint, Build, Assign Reviewer Workflow 생성 (#13)
    * docs: assign reviewer, build workflow 생성
    
    * docs: lint step 추가
    
    * chore: yml file name change
    
    * docs: cache pnpm 삭제
    
    * chore: 모든 Page 임시 템플릿 생성
    
    * docs: artifact path 수정
    
    * docs: artifact path: next
    
    * docs: artifact path: .next
    
    * docs: target branch 제거
    
    * chore: biome lint workflow test
    
    * docs: run biome ci for ci environment
    
    * chore: console.log 삭제
    
    * docs: biome ci formatter disabled test
    
    * docs: lint error return test
    
    * chore: biome test 코드 삭제
    
    * chore: noBarrelFile, noReExportAll off
    
    * docs: type check by tsc + seperate cd pipeline
    
    * docs: concurrently check 생성하여 workflow 반영
    
    * docs: name 수정
    
    * fix: biome lint error resolve
    
    * docs: pnpm lock file update
    
    * chore: check script ci로 수정하고 biome ignore 추가

--- svgr.d.ts
+++ svgr.d.ts
@@ -1,12 +1,11 @@
-
-declare module '*.svg' {
-  import type { FC, SVGProps } from 'react';
+declare module "*.svg" {
+  import type { FC, SVGProps } from "react";
   const content: FC<SVGProps<SVGElement>>;
   export default content;
 }
 
-declare module '*.svg?url' {
-  import { StaticImageData } from 'next/image';
+declare module "*.svg?url" {
+  import type { StaticImageData } from "next/image";
   const content: StaticImageData;
   export default content;
 }
