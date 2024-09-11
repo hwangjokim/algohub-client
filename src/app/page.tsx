909157d3461af752ec3aff806f8a3f87ee144f13
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-12 08:42:16 +0900

    [ Feat ] lottie 세팅 (#18)
    * init: gif, lottie json 등 나머지 assets 추가
    
    * feat: lottie animation 재생 컴포넌트 추가
    
    * chore: public 디렉토리 수정
    
    * init: 누락된 assets 추가
    
    ---------
    
    Co-authored-by: Lee jin <83453646+j-nary@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 import { IcnAlarm } from "@/assets/svgs";
 import AlarmUrl from "@/assets/svgs/icn_alarm.svg?url";
-import Button from "@/components/common/Button/Button";
-import Image from "next/image";
 import "@/styles/globalStyles.css";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <IcnAlarm width={50} height={50} />
-      <Image width={50} height={50} src={AlarmUrl} alt="alarm" priority />
-      <img width={50} height={50} src={AlarmUrl.src} alt="alarm" />
-      <div
-        style={{
-          width: 50,
-          height: 50,
-          backgroundImage: `url(${AlarmUrl.src})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      />
-      <Button text="버튼" />
-    </div>
-  );
+	return (
+		<div>
+			<IcnAlarm width={50} height={50} />
+			<Image width={50} height={50} src={AlarmUrl} alt="alarm" priority />
+			<img width={50} height={50} src={AlarmUrl.src} alt="alarm" />
+			<div
+				style={{
+					width: 50,
+					height: 50,
+					backgroundImage: `url(${AlarmUrl.src})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+				}}
+			/>
+		</div>
+	);
 }
