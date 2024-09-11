021a68271c5624351e38d4f37e4f154b66b1a674
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-12 08:31:41 +0900

    chore: public 디렉토리 수정

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        ["@svgr/webpack"],
+        use: ["@svgr/webpack"],
       }
     );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
-import { arrow, exitGroup, requireLogin } from "@/assets/lottie";
 import { IcnAlarm } from "@/assets/svgs";
 import AlarmUrl from "@/assets/svgs/icn_alarm.svg?url";
-import Image from "next/image";
 import "@/styles/globalStyles.css";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <IcnAlarm width={50} height={50} />
-      <Image width={50} height={50} src={AlarmUrl} alt="alarm" priority />
-      <img width={50} height={50} src={AlarmUrl.src} alt="alarm" />
-      <div
-        style={{
-          width: 50,
-          height: 50,
-          backgroundImage: `url(${AlarmUrl.src})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      />
-      <Button text="버튼" />
-    </div>
-  );
+	return (
+		<div>
+			<IcnAlarm width={50} height={50} />
+			<Image width={50} height={50} src={AlarmUrl} alt="alarm" priority />
+			<img width={50} height={50} src={AlarmUrl.src} alt="alarm" />
+			<div
+				style={{
+					width: 50,
+					height: 50,
+					backgroundImage: `url(${AlarmUrl.src})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+				}}
+			/>
+		</div>
+	);
 }
