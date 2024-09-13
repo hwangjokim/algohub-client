0fcdf007fcaa1d2c9da745b3fdbba629c50921c2
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-13 19:51:37 +0900

    feat: header dropdown 클릭 기능 구현

--- src/components/common/Header/Header.css.ts
+++ src/components/common/Header/Header.css.ts
@@ -4,19 +4,20 @@ import { style } from "@vanilla-extract/css";
 
 export const headerStyle = style({
 	display: "flex",
-	width: "100dvw",
-	height: "7.2rem",
 	justifyContent: "space-between",
 	alignItems: "center",
-	padding: "0 6.4rem",
+
+	width: "100vw",
+	height: "7.2rem",
 	margin: 0,
+	padding: "0 6.4rem",
 	backgroundColor: theme.color.bg,
-	boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
 });
 
 export const logoContainer = style({
 	display: "flex",
 	justifyContent: "flex-start",
+
 	margin: 0,
 	cursor: "pointer",
 });
@@ -29,22 +30,92 @@ export const logoStyle = style({
 export const buttonContainer = style({
 	display: "flex",
 	justifyContent: "flex-end",
-	padding: 0,
 	gap: "1.6rem",
+
 	margin: 0,
+	padding: 0,
 });
 
 export const buttonStyle = style({
+	position: "relative",
 	width: "2.5rem",
 	height: "2.5rem",
-	borderRadius: "50px",
+	borderRadius: "5rem",
 	cursor: "pointer",
 	":hover": {
-		backgroundColor: theme.color.purple2,
+		backgroundColor: theme.color.mg5,
 	},
 });
 
 export const iconStyle = style({
 	width: "inherit",
 	height: "inherit",
+});
+
+export const countStyle = style({
+	position: "absolute",
+	right: 0,
+	width: "1.2rem",
+	height: "1.2rem",
+	borderRadius: "1rem",
+	backgroundColor: theme.color.purple2,
+	color: theme.color.white,
+	alignContent: "center",
+	fontSize: "0.8rem",
+	fontWeight: 500,
+	lineHeight: "5.6px",
+	letterSpacing: "-0.025em",
+	textAlign: "center",
+});
+
+export const notificationContainer = style({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "flex-start",
+	gap: "1rem",
+	position: "absolute",
+	top: "7.2rem",
+	right: "8rem",
+
+	width: "26.9rem",
+	height: "27.4rem",
+	padding: "0.8rem",
+	opacity: "0.9",
+	borderRadius: "2rem",
+	backdropFilter: "blur(2px)",
+	backgroundColor: theme.color.mg6,
+});
+
+export const profileMenuContainer = style({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "flex-start",
+	gap: "1rem",
+	position: "absolute",
+	top: "6.5rem",
+
+	width: "13.2rem",
+	height: "8.8rem",
+	padding: "0.8rem 1rem",
+	borderRadius: "0.8rem",
+	backdropFilter: "blur(2px)",
+	backgroundColor: theme.color.mg6,
+});
+
+export const profileMenuItemStyle = style({
+	listStyle: "none",
+	width: "11.2rem",
+	height: "3.4rem",
+	borderRadius: "0.8rem",
+	padding: "0.5rem 0.8rem",
+
+	color: theme.color.white,
+	textAlign: "left",
+	...theme.font["caption-m-12"],
+
+	selectors: {
+		"&:hover": {
+			backgroundColor: theme.color.mg5,
+		},
+	},
 });
--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -3,17 +3,22 @@
 import { IcnBellHeader, IcnLogo } from "@/assets/svgs";
 import IcnNew from "@/assets/svgs/icn_new.svg?url";
 import { authAtom } from "@/store";
+import { checkContains } from "@/utils/domUtils";
 import { useAtomValue } from "jotai";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
 	buttonContainer,
 	buttonStyle,
+	countStyle,
 	headerStyle,
 	iconStyle,
 	logoContainer,
 	logoStyle,
+	notificationContainer,
+	profileMenuContainer,
+	profileMenuItemStyle,
 } from "./Header.css";
 
 const Header = () => {
@@ -24,46 +29,120 @@ const Header = () => {
 			<Link href={"/user"} className={logoContainer} aria-label="User page">
 				<IcnLogo className={logoStyle} />
 			</Link>
-			{isLoggedIn ? <UserMenu /> : <LoginMenu />}
+			{/* TODO: 로그인 api 부착 후 isLoggedIn으로 교체 */}
+			{true ? <UserMenu /> : <LoginMenu />}
 		</header>
 	);
 };
 
 const UserMenu = () => {
-	const [showNotification, setShowNotification] = useState(false);
+	const [showNotifications, setShowNotifications] = useState(false);
 	const [showProfileMenu, setShowProfileMenu] = useState(false);
+	const notificationBtnRef = useRef<HTMLButtonElement | null>(null);
+	const notificationRef = useRef<HTMLUListElement | null>(null);
+	const profileBtnRef = useRef<HTMLButtonElement | null>(null);
+	const profileMenuRef = useRef<HTMLDivElement | null>(null);
+	const count = 10;
+	const handleNotificationClick = useCallback(() => {
+		setShowNotifications(!showNotifications);
+		// 스크린 리더로 열 때 옆 메뉴 닫기
+		setShowProfileMenu(false);
+	}, [showNotifications]);
 
-	const handleNotificationClick = () => setShowNotification(!showNotification);
-	const handleProfileClick = () => setShowProfileMenu(!showProfileMenu);
+	const handleProfileClick = useCallback(() => {
+		setShowProfileMenu(!showProfileMenu);
+		// 스크린 리더로 열 때 옆 메뉴 닫기
+		setShowNotifications(false);
+	}, [showProfileMenu]);
+
+	const handleOutsideClick = useCallback(({ target }: MouseEvent) => {
+		// 알림, 프로필 버튼, 드롭다운 외의 화면 클릭 시 드롭다운 닫기
+		const [notiBtn, noti, profileBtn, profile] = checkContains(
+			target!,
+			notificationBtnRef,
+			notificationRef,
+			profileBtnRef,
+			profileMenuRef,
+		);
+		if (!(notiBtn || noti)) {
+			setShowNotifications(false);
+		}
+		if (!(profileBtn || profile)) {
+			setShowProfileMenu(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		// click대신 mousedown으로 빠르게 진행
+		document.addEventListener("mousedown", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, [handleOutsideClick]);
 
 	return (
 		<div className={buttonContainer}>
 			<button
+				ref={notificationBtnRef}
 				className={buttonStyle}
-				id="notificationDropdownToggle"
-				aria-label="Show notifications"
-				aria-expanded={showNotification}
-				aria-controls="notificationDropdown"
+				id="notificationToggle"
+				aria-label={`${showNotifications ? "Hide" : "Show"} notifications`}
+				aria-expanded={showNotifications}
+				aria-controls="notifications"
 				onClick={handleNotificationClick}
 			>
+				{count > 0 && <div className={countStyle}>{count}</div>}
 				<IcnBellHeader className={iconStyle} />
 			</button>
+
+			{/* TODO: 알림창 구현 */}
+			{showNotifications && (
+				<ul
+					ref={notificationRef}
+					id="notifications"
+					className={notificationContainer}
+					role="alert"
+				/>
+			)}
+
 			<button
+				ref={profileBtnRef}
 				className={buttonStyle}
-				id="profileDropdownToggle"
-				aria-label="Open user profile menu"
+				id="profileMenuToggle"
+				aria-label={`${showProfileMenu ? "Close" : "Open"} user profile menu`}
 				aria-expanded={showProfileMenu}
-				aria-controls="profileDropdown"
+				aria-controls="profileMenu"
 				onClick={handleProfileClick}
 			>
 				<Image className={iconStyle} src={IcnNew} alt="user profile" priority />
 			</button>
+
+			{showProfileMenu && (
+				<div
+					ref={profileMenuRef}
+					className={profileMenuContainer}
+					id="profileMenu"
+					aria-label="user profile menu"
+					role="menu"
+				>
+					{/* TODO: google profile 디자인 대기 중 */}
+					<button className={profileMenuItemStyle} role="menuitem">
+						내 프로필
+					</button>
+					<button className={profileMenuItemStyle} role="menuitem">
+						로그아웃
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
 
 const LoginMenu = () => {
-	const handleProfileClick = () => {};
+	const handleProfileClick = () => {
+		alert("로그인");
+	};
 
 	return (
 		<div className={buttonContainer}>
