aa0659a6b8651b695d41ea33a2a0e1ad74e66900
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-14 02:39:40 +0900

    docs: 불필요한 파일 제거

--- src/app/style.css.ts
+++
@@ -1,8 +0,0 @@
-import { theme } from "@/styles/themes.css";
-import { style } from "@vanilla-extract/css";
-
-export const icnPlusStyle = style({
-  '&:path': {
-    fill: theme.color.white,
-  }
-})
