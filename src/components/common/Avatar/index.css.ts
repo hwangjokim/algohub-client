81241fcaee3ead6af3fcd9af924613e8c778503b
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-15 02:23:15 +0900

    [ Feat ] 공통 컴포넌트 Avatar 구현 (#28)
    * feat: Avatar 컴포넌트 구현
    
    * feat: userIcon 추가
    
    * feat: role 속성 추가
    
    * refactor: 코드리뷰 반영
    
    * refactor: 인터페이스 변경
    
    * refactor: 키 밸류 자동 삽입
    
    * refactor: imageProps 내려받기
    
    * refactor: 버튼 태그 삭제
    
    * refactor: 불필요한 인터페이스 삭제
    
    * refactor: Image 태그 role 추가
    
    * refactor: inset 추가
    
    * fix: 혼날뻔
    
    * refactor: inset 되돌리기

---
+++ src/components/common/Avatar/index.css.ts
@@ -0,0 +1,41 @@
+import { theme } from "@/styles/themes.css";
+import { style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+
+export const wrapperStyle = style({
+  position: 'relative',
+  width: 'fit-content'
+})
+
+export const avatarStyle = recipe({
+  base: {
+    borderRadius: '50%',
+    overflow: 'hidden',
+  },
+  variants: {
+    size: {
+      mini: {
+        width: '2.5rem',
+        height: '2.5rem'
+      },
+      small: {
+        width: '3.9rem',
+        height: '3.9rem'
+      },
+      medium: {
+        width: '8.2rem',
+        height: '8.2rem'
+      },
+      large: {
+        width: '12rem',
+        height: '12rem'
+      }
+    },
+    hasShadow: {
+      true: {
+        border: `0.5px solid ${theme.color.purple}`,
+        boxShadow: `0px 0px 4px 0px ${theme.color.purple}`
+      }
+    }
+  }
+})
---
+++ src/components/group/MemberAvatar/index.tsx
@@ -0,0 +1,18 @@
+import { IcnUser2 } from "@/assets/svgs";
+import Avatar from "@/components/common/Avatar";
+import { iconStyle } from "@/components/group/MemberAvatar/index.css";
+import type { StaticImageData } from "next/image";
+
+interface MemberAvatarProps {
+  isLeader: boolean;
+  src: StaticImageData;
+}
+const MemberAvatar = ({ isLeader, src }: MemberAvatarProps) => {
+  return (
+    <Avatar src={src} size="small" hasShadow alt="멤버 프로필">
+      {isLeader && <IcnUser2 className={iconStyle} />}
+    </Avatar>
+  );
+};
+
+export default MemberAvatar;
