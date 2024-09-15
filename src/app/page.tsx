ed6614798658c23b97d6f5d48051db7c9c4deecf
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-16 00:14:40 +0900

    fix: CI 빌드 에러 해결 시도 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ const OnboardingPage = () => {
   return (
     <div>
       OnboardingPage
-      <Animation size={""} animationJson={undefined} />
+      {/* <Animation size={""} animationJson={undefined} />
       <ToggleButton
         isSelected={false}
         onChange={function (): void {
           throw new Error("Function not implemented.");
         }}
-      />
+      /> */}
     </div>
   );
 };
