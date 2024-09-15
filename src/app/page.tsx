01be992369925fb50c49fd651642d683204543f4
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-16 00:13:07 +0900

    fix: CI 빌드 에러 해결 시도

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Animation from "@/common/component/Animation";
+import ToggleButton from "@/common/component/ToggleButton";
 import "@/styles/globalStyles.css";
 
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  return (
+    <div>
+      OnboardingPage
+      <Animation size={""} animationJson={undefined} />
+      <ToggleButton
+        isSelected={false}
+        onChange={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    </div>
+  );
 };
 
 export default OnboardingPage;
