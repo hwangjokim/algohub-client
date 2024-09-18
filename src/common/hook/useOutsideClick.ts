563c7e004eb659e304f3ad8ed32d7b68208488ae
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-09-18 21:13:33 +0900

    refactor: useOutsideClick 훅 esc키로도 작동하게 수정

--- src/common/hook/useOutsideClick.ts
+++ src/common/hook/useOutsideClick.ts
@@ -9,17 +9,27 @@ import { useCallback, useEffect, useRef } from "react";
  */
 export const useOutsideClick = (handleToggle: () => void) => {
   const ref = useRef<HTMLDivElement>(null);
-  const handleOutsideClick = useCallback(({ target }: MouseEvent) => {
-    const [check] = checkContains(target!, ref);
-    if (!check) handleToggle();
-  }, [handleToggle]);
+  const handleOutsideClick = useCallback(
+    ({ target }: MouseEvent) => {
+      const [check] = checkContains(target!, ref);
+      if (!check) handleToggle();
+    },
+    [handleToggle],
+  );
+  const handleESCKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Escape") handleToggle();
+    },
+    [handleToggle],
+  );
 
   useEffect(() => {
     // click대신 mousedown으로 빠르게 진행
     document.addEventListener("mousedown", handleOutsideClick);
-
+    document.addEventListener("keydown", handleESCKeyDown);
     return () => {
       document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleESCKeyDown);
     };
   }, [handleOutsideClick]);
 
