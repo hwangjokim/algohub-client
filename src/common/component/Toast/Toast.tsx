4c3a6c5183a1cf1390b03ce51cde2770d5067d26
wuzoo <wndyd9911@gmail.com>
2024-09-21 21:36:30 +0900

    feat: renderRef를 추가하여 null로 업데이트 시 렌더링 이슈 해결

--- src/common/component/Toast/Toast.tsx
+++ src/common/component/Toast/Toast.tsx
@@ -1,7 +1,10 @@
 import { IcnCheckSmall, IcnError } from "@/asset/svg";
 import { textStyle, toastStyle } from "@/common/component/Toast/index.css";
 import { useTimeout } from "@/common/hook/useTimout";
-import { type PropsWithChildren, useState } from "react";
+import { toastAtom } from "@/shared/store/toast";
+import type { Timeout } from "@/shared/type";
+import { useSetAtom } from "jotai";
+import { type PropsWithChildren, useRef, useState } from "react";
 
 type ToastProps = PropsWithChildren<{
   variant?: "error" | "success" | "default";
@@ -13,7 +16,21 @@ const Toast = ({
   duration = 2000,
   children,
 }: ToastProps) => {
-  useTimeout(() => setAnimation("hide"), duration);
+  /** atom이 바로 null로 바뀌면서 생기는 렌더링 이슈를 제어할 ref */
+  const renderRef = useRef<Timeout>();
+
+  const setAtom = useSetAtom(toastAtom);
+
+  useTimeout(() => {
+    setAnimation("hide");
+
+    /** hide 상태로 돌입하고 바로 업데이트하지 않고 600 대기 */
+    renderRef.current = setTimeout(() => {
+      setAtom({ toast: null });
+    }, 600);
+
+    clearTimeout(renderRef.current);
+  }, duration);
 
   const [animation, setAnimation] = useState<"show" | "hide">("show");
 
