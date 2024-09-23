f8b449714c574bdf05597f26978a501518c66cd5
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-24 04:57:40 +0900

    feat: focus trap 추가

--- src/common/component/Modal/index.tsx
+++ src/common/component/Modal/index.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import {
   backgroundStyle,
   dialogStyle,
 } from "@/common/component/Modal/index.css";
 import { useOutsideClick } from "@/common/hook/useOutsideClick";
-import type { ComponentPropsWithoutRef } from "react";
+import { useEffect, useRef, type ComponentPropsWithoutRef } from "react";
 import { createPortal } from "react-dom";
 
 interface ModalProps extends ComponentPropsWithoutRef<"dialog"> {
@@ -13,6 +15,37 @@ interface ModalProps extends ComponentPropsWithoutRef<"dialog"> {
 
 export const Modal = ({ isOpen, onClose, children, ...props }: ModalProps) => {
   const closeRef = useOutsideClick<HTMLDialogElement>(onClose);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Tab") {
+        const focusableElements = closeRef.current?.querySelectorAll(
+          'a, button, input, textarea, select, [tabindex]:not([tabindex="-1"])'
+        );
+        if (!focusableElements) return;
+
+        const firstElement = focusableElements[0] as HTMLElement;
+        const lastElement = focusableElements[
+          focusableElements.length - 1
+        ] as HTMLElement;
+
+        if (e.shiftKey && document.activeElement === firstElement) {
+          e.preventDefault();
+          lastElement.focus();
+        } else if (!e.shiftKey && document.activeElement === lastElement) {
+          e.preventDefault();
+          firstElement.focus();
+        }
+      }
+    };
+
+    if (isOpen) document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
+
   return (
     <>
       {isOpen &&
