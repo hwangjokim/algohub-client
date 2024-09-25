0d32bff6a76049d79285fc36b096e05a31fd1549
wuzoo <wndyd9911@gmail.com>
2024-09-25 23:24:45 +0900

    feat: motion layoutId로 transition 시 공통된 애니메이션 적용

--- src/common/component/Tab/Indicator.tsx
+++ src/common/component/Tab/Indicator.tsx
@@ -1,10 +1,16 @@
 import { useTabState } from "@/common/component/Tab/TabProvider";
 import { indicatorStyle } from "@/common/component/Tab/index.css";
+import { motion } from "framer-motion";
 
 const Indicator = () => {
   const { variant } = useTabState();
 
-  return <div className={indicatorStyle({ variant: variant })} />;
+  return (
+    <motion.div
+      layoutId="tab-indicator"
+      className={indicatorStyle({ variant: variant })}
+    />
+  );
 };
 
 export default Indicator;
