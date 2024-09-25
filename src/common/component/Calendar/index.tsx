f71a72a0d82c7611011583dd2603bf5e13c7bbab
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2024-09-25 22:36:35 +0900

    [ Feat ] Datepicker 구현  (#51)
    * react-datepicker test v1
    
    * feat: Implement datepicker
    
    * refactor: Make props more general
    
    * chore: CSS 정리
    
    * add : Add component to story
    
    ---------
    
    Co-authored-by: Lee jin <83453646+j-nary@users.noreply.github.com>

---
+++ src/common/component/Calendar/index.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState } from "react";
+import DatePicker, {
+  type DatePickerProps,
+  registerLocale,
+} from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { IcnBtnArrowLeft, IcnCalenderCard } from "@/asset/svg";
+import { ko } from "date-fns/locale";
+import {
+  arrowWrapperStyle,
+  calendarIcnStyle,
+  customHeaderWrapperStyle,
+  dateDetailStyle,
+  dateStyle,
+  leftArrowStyle,
+  wrapperStyle,
+} from "./index.css";
+registerLocale("ko", ko);
+
+interface CalendarProps
+  extends Omit<
+    DatePickerProps,
+    "onChange" | "selectsRange" | "selectsMultiple"
+  > {
+  onChange: (date: Date) => void;
+}
+
+const Calendar = ({ onChange, ...props }: CalendarProps) => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(
+    props.startDate ?? new Date()
+  );
+  const [selected, setSelected] = useState<boolean>(false);
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+    setSelected(true);
+    if (date) {
+      onChange(date);
+    }
+  };
+
+  return (
+    <div className={wrapperStyle}>
+      <div>
+        <DatePicker
+          dateFormat="yyyy.MM.dd"
+          selected={selectedDate}
+          className={dateStyle({ selected })}
+          renderCustomHeader={renderCustomHeader}
+          onChange={handleDateChange}
+          locale="ko"
+          calendarStartDay={1}
+          popperPlacement="bottom-start"
+          shouldCloseOnSelect
+          {...props}
+        />
+      </div>
+
+      <IcnCalenderCard className={calendarIcnStyle({ selected })} />
+    </div>
+  );
+};
+
+interface CustomHeaderProps {
+  date: Date;
+  decreaseMonth: () => void;
+  increaseMonth: () => void;
+}
+
+const renderCustomHeader = ({
+  date,
+  decreaseMonth,
+  increaseMonth,
+}: CustomHeaderProps) => {
+  return (
+    <div className={customHeaderWrapperStyle}>
+      <button className={arrowWrapperStyle} onClick={decreaseMonth}>
+        <IcnBtnArrowLeft className={leftArrowStyle({ rotate: false })} />
+      </button>
+      <div className={dateDetailStyle}>{`${date.getFullYear()}년 ${
+        date.getMonth() + 1
+      }월`}</div>
+      <button className={arrowWrapperStyle} onClick={increaseMonth}>
+        <IcnBtnArrowLeft className={leftArrowStyle({ rotate: true })} />
+      </button>
+    </div>
+  );
+};
+
+export default Calendar;
