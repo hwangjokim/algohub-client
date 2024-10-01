b7ef7d2ebc4ae81af6ea4830ac11061fc4b8bdd5
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-02 02:04:09 +0900

    Squashed commit of the following:
    commit 7c0dda6e142c5f67d7ca1f258894bd70dbd2e49a
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 29 20:58:55 2024 +0900
    
        style: Seperate supportingText from  components
    
    commit 5a2df10b36f9a116f6f250552f1c932ba202c6d2
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 29 20:58:05 2024 +0900
    
        style: Seperate supportingText from  components
    
    commit 27766d28ec01eb06ad1b95dc03505b6806297cec
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sat Sep 28 22:51:38 2024 +0900
    
        style: Change focus-within to focus
    
    commit 8e7e43e8588d078409aa071eda3202abf936ef57
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sat Sep 28 22:34:00 2024 +0900
    
        Remove useless fragment
    
    commit 25ec259d06715617f9b39e71ff128444d07062a1
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sat Sep 28 22:30:22 2024 +0900
    
        Remove unused code
    
    commit 6417797c1f03b99770ed6db12b7f03be5083b792
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sat Sep 28 22:26:47 2024 +0900
    
        Apply comments
    
    commit cba01b31d427c4c05c33d46e7844c81b6e814867
    Merge: 8b997e9 6582c59
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 22:18:43 2024 +0900
    
        Merge branch 'main' into init/#5/input-component
    
    commit 8b997e97c53b15f359ec700b16ced269680480da
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 22:16:25 2024 +0900
    
        chore: Organize imports and add story
    
    commit 86b01d24c184125ac4d7fafac07d247dada22d40
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 21:38:05 2024 +0900
    
        style: bar 제거 및 ErrorInfo 컴포넌트 분리
    
    commit 2406e3cd9e75e5d89a0e37ebba09f5bdb687b5d0
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 01:58:53 2024 +0900
    
        chore: update textarea's figma url
    
    commit 6568fae567ce8a5dad26ac50f496cf678d27784a
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 01:52:43 2024 +0900
    
        Add : Add component's to storyBook
    
    commit fe045406a4fea3c06cda1f7a52db88577a0a32d7
    Author: hwangjokim <hg7258@naver.com>
    Date:   Sun Sep 22 01:44:41 2024 +0900
    
        refactor: Apply comments
    
    commit a3e8089a07dd8e29ea9449275cfc0da1b1b78511
    Author: hwangjokim <hg7258@naver.com>
    Date:   Wed Sep 18 16:53:58 2024 +0900
    
        fix: add 'use client' and update prop types
    
    commit 1087c620f50abb9f85287c6389de01eeee273a09
    Author: hwangjokim <hg7258@naver.com>
    Date:   Wed Sep 18 01:12:09 2024 +0900
    
        feat: Add common-input component

---
+++ src/common/component/Textarea/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import {
+  type ForwardedRef,
+  type TextareaHTMLAttributes,
+  forwardRef,
+} from "react";
+import { textareaStyle } from "./index.css";
+
+export interface TextareaProps
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  isError?: boolean;
+}
+
+const Textarea = forwardRef(
+  (
+    { isError = false, ...props }: TextareaProps,
+    ref: ForwardedRef<HTMLTextAreaElement>
+  ) => {
+    return (
+      <textarea
+        ref={ref}
+        className={textareaStyle({ isError })}
+        aria-invalid={isError}
+        aria-multiline="true"
+        {...props}
+      />
+    );
+  }
+);
+
+export default Textarea;
