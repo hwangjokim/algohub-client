f7addd53548f7d9211f2516636eb48f33a52b80f
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-04 02:40:06 +0900

    fix: format

--- src/shared/hook/useIntersectionObserver.ts
+++ src/shared/hook/useIntersectionObserver.ts
@@ -1,14 +1,13 @@
 import { useEffect, useRef } from "react";
 
-export const useFadeIn = <T extends HTMLElement>(callback: IntersectionObserverCallback) => {
+export const useFadeIn = <T extends HTMLElement>(
+  callback: IntersectionObserverCallback,
+) => {
   const imageRef = useRef<T>(null);
 
   useEffect(() => {
     if (!imageRef.current) return;
-    const observer = new IntersectionObserver(
-      callback,
-      { threshold: 0.1 },
-    );
+    const observer = new IntersectionObserver(callback, { threshold: 0.1 });
 
     observer.observe(imageRef.current);
 
