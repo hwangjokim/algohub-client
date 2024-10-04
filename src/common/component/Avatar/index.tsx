6e22f3e17f3e6758cfd985ab20468b0515a4b8d5
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-05 04:02:17 +0900

    docs: clsx + JSDoc 적용

--- src/common/component/Avatar/index.tsx
+++ src/common/component/Avatar/index.tsx
@@ -1,19 +1,23 @@
 import { avatarStyle, wrapperStyle } from "@/common/component/Avatar/index.css";
+import { clsx } from "clsx";
 import Image, { type ImageProps } from "next/image";
 
 interface AvatarProps extends ImageProps {
+  /** mini: '2.5rem'<br/>small: '3.9rem'<br/>medium: '8.2rem'<br/>large: '12rem' */
   size?: "mini" | "small" | "medium" | "large";
+  /** 보라색 그림자 표시 여부 */
   hasShadow?: boolean;
 }
 
 const Avatar = ({
+  className,
   size = "medium",
   hasShadow = false,
   children,
   ...imageProps
 }: AvatarProps) => {
   return (
-    <div className={wrapperStyle}>
+    <div className={clsx(wrapperStyle, className)}>
       <Image
         {...imageProps}
         role="button"
