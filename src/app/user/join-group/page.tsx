bc312fc3b47d4929d2143a017052d2167b867f26
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-05 04:48:25 +0900

    feat: Modal Container 구현

--- src/app/user/join-group/page.tsx
+++ src/app/user/join-group/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+import UserDashboardPage from "@/app/user/page";
+import { IcnBtnDelete } from "@/asset/svg";
+import Modal from "@/common/component/Modal";
+import { exitStyle, wrapper } from "@/view/user/join-group/component/index.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const JoinGroupPage = () => {
-  return <div>JoinGroupPage</div>;
+  const router = useRouter();
+  return (
+    <>
+      <UserDashboardPage />
+      <Modal isOpen={true} onClose={() => router.push("/user")}>
+        <section className={wrapper}>
+          <Link href="/user">
+            <IcnBtnDelete width={10} height={10} className={exitStyle} />
+          </Link>
+        </section>
+      </Modal>
+    </>
+  );
 };
 
 export default JoinGroupPage;
---
+++ src/view/user/join-group/component/index.css.ts
@@ -0,0 +1,23 @@
+import { theme } from "@/styles/themes.css";
+import { style } from "@vanilla-extract/css";
+
+export const wrapper = style({
+  display: "flex",
+  justifyContent: "center",
+  flexShrink: "0",
+
+  position: "relative",
+
+  width: "54rem",
+  height: "65.6rem",
+  padding: "7.4rem 2.2rem 2.6rem",
+
+  borderRadius: "16px",
+  backgroundColor: theme.color.bg,
+});
+
+export const exitStyle = style({
+  position: "absolute",
+  top: "3.3rem",
+  right: "3.3rem",
+});
