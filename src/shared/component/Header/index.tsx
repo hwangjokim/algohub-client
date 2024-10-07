905fe078c55e8acd9f38375eb02903ad334e3a55
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-08 01:15:57 +0900

    fix: use client 적용 범위 축소

--- src/shared/component/Header/index.tsx
+++ src/shared/component/Header/index.tsx
@@ -1,5 +1,4 @@
 import { IcnLogo } from "@/asset/svg";
-import LoginMenu from "@/shared/component/Header/LoginMenu";
 import UserMenu from "@/shared/component/Header/UserMenu";
 import {
   headerStyle,
@@ -8,19 +7,13 @@ import {
 } from "@/shared/component/Header/index.css";
 import Link from "next/link";
 
-const Header = () => {
-  /* TODO: 로그인 api 부착 후 Atom으로 교체 */
-  // const { isLoggedIn } = useAtomValue(authAtom);
-  const isLoggedIn = false;
-
-  return (
-    <header className={headerStyle}>
-      <Link href={"/user"} className={logoContainer} aria-label="User page">
-        <IcnLogo className={logoStyle} aria-label="algoHub 로고" />
-      </Link>
-      {isLoggedIn ? <UserMenu /> : <LoginMenu />}
-    </header>
-  );
-};
+const Header = () => (
+  <header className={headerStyle}>
+    <Link href={"/user"} className={logoContainer} aria-label="User page">
+      <IcnLogo className={logoStyle} aria-label="algoHub 로고" />
+    </Link>
+    <UserMenu />
+  </header>
+);
 
 export default Header;
