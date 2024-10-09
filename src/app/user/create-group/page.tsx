bf85762225999b5a801ef0da9313a9d16a679348
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-10 00:41:16 +0900

    chore: Modal 수정사항 반영

--- src/app/user/create-group/page.tsx
+++ src/app/user/create-group/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 import UserDashboardPage from "@/app/user/page";
-import { IcnBtnDelete } from "@/asset/svg";
 import Modal from "@/common/component/Modal";
 import { useToast } from "@/common/hook/useToast";
 import CreateGroupForm from "@/view/user/create-group/component/CreateGroupForm";
 import LinkWithCopy from "@/view/user/create-group/component/LinkWithCopy";
-import {
-  exitStyle,
-  wrapper,
-} from "@/view/user/create-group/component/index.css";
-import Link from "next/link";
+import { wrapper } from "@/view/user/create-group/component/index.css";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -27,17 +22,8 @@ const CreateGroupPage = () => {
   return (
     <>
       <UserDashboardPage />
-      <Modal isOpen={true} onClose={() => router.push("/user")}>
+      <Modal isOpen={true} onClose={() => router.push("/user")} hasCloseBtn>
         <div className={wrapper}>
-          <Link href="/user">
-            <IcnBtnDelete
-              width={10}
-              height={10}
-              className={exitStyle}
-              aria-label="Close JoinGroup Modal"
-              role="button"
-            />
-          </Link>
           <CreateGroupForm setIsSuccess={setIsSuccess} />
           {isSuccess && <LinkWithCopy link="algohub.kr" />}
         </div>
