a52d6af8d1e3a5e94ec308ae65327ae02acdeb96
wuzoo <wndyd9911@gmail.com>
2024-10-12 14:43:27 +0900

    docs: metadata 추가

--- src/app/group/layout.tsx
+++ src/app/group/layout.tsx
@@ -1,5 +1,10 @@
-import { IcnPlus, IcnSearch, IcnSquare } from "@/asset/svg";
 import NavBar from "@/shared/component/NavBar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "그룹 페이지",
+  description: "알고리즘 스터디 플랫폼 그룹 페이지",
+};
 
 export default function GroupLayout({
   children,
@@ -9,17 +14,13 @@ export default function GroupLayout({
   return (
     <main>
       <NavBar>
-        <NavBar.Item icon={<IcnSquare />} mode="fill" href="/group">
+        <NavBar.Item mode="fill" href="/group">
           대시보드
         </NavBar.Item>
-        <NavBar.Item
-          icon={<IcnSearch />}
-          mode="stroke"
-          href="/group/solved-list"
-        >
+        <NavBar.Item mode="stroke" href="/group/solved-list">
           문제 리스트
         </NavBar.Item>
-        <NavBar.Item icon={<IcnPlus />} mode="stroke" href="/group/setting">
+        <NavBar.Item mode="stroke" href="/group/setting">
           스터디 관리
         </NavBar.Item>
       </NavBar>
