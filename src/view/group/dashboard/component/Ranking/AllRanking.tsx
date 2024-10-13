d72aabf55500ad2d33dec879507a3b013b4672e6
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-13 09:20:14 +0900

    feat: 전체 랭킹 구현

--- src/view/group/dashboard/component/Ranking/AllRanking.tsx
+++ src/view/group/dashboard/component/Ranking/AllRanking.tsx
@@ -1,5 +1,22 @@
+import type { GroupRankingResponse } from "@/api/group/type";
+import RankList from "@/view/group/dashboard/component/Ranking/RankList";
+import { allRankingWrapper } from "@/view/group/dashboard/component/Ranking/index.css";
+
 const AllRanking = () => {
-  return <div>AllRanking</div>;
+  const tmpData: GroupRankingResponse = {
+    userNickname: "달리는 지니",
+    profileImage: "",
+    rank: 1,
+    solvedCount: 38,
+  };
+
+  return (
+    <ol className={allRankingWrapper}>
+      {[tmpData, tmpData, tmpData].map((data, idx) => (
+        <RankList key={idx} info={data} />
+      ))}
+    </ol>
+  );
 };
 
 export default AllRanking;
--- src/view/group/dashboard/component/Ranking/index.css.ts
+++ src/view/group/dashboard/component/Ranking/index.css.ts
@@ -55,3 +55,77 @@ export const nicknameStyle = style({
   color: theme.color.purple2,
   ...theme.font.Title1_SB_16,
 });
+
+export const allRankingWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1.5rem",
+
+  paddingTop: "2.4rem",
+});
+
+export const listWrapper = style({
+  display: "flex",
+  alignItems: "center",
+  gap: "1.6rem",
+
+  width: "100%",
+
+  ":hover": {
+    backgroundColor: theme.color.mg5,
+  },
+});
+
+export const listContainer = style({
+  display: "flex",
+  justifyContent: "space-between",
+
+  width: "100%",
+});
+
+export const rankWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.4rem",
+});
+
+export const infoWrapper = style({
+  display: "flex",
+  gap: "0.5rem",
+});
+
+export const rankTextStyle = style({
+  color: theme.color.purple,
+  ...theme.font.Caption3_M_12,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+});
+
+export const allNicknameStyle = style({
+  color: theme.color.white,
+  ...theme.font.Caption2_SB_12,
+});
+
+export const solvedTextStyle = style({
+  color: theme.color.purple2,
+  ...theme.font.Body3_SB_14,
+});
+
+export const changeStyle = recipe({
+  base: {
+    ...theme.font.Caption1_R_12,
+  },
+  variants: {
+    change: {
+      up: {
+        color: theme.color.red,
+      },
+      down: {
+        color: theme.color.blue,
+      },
+      unChanged: {
+        color: theme.color.mg4,
+      },
+    },
+  },
+});
