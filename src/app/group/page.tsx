c9d64777c75bf2d10c8fa62bd3d2c50b24b0f4a2
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-13 11:25:43 +0900

    feat: toast 추가

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -2,15 +2,19 @@
 
 import {} from "@/asset/svg";
 import Modal from "@/common/component/Modal";
+import ToastProvider from "@/common/component/Toast";
 import RegisterForm from "@/view/group/problem-list/component/RegisterForm";
 import { useState } from "react";
 
 const GroupDashboardPage = () => {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-      <RegisterForm />
-    </Modal>
+    <>
+      <ToastProvider />
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <RegisterForm />
+      </Modal>
+    </>
   );
 };
 
--- src/view/group/problem-list/component/RegisterForm/index.tsx
+++ src/view/group/problem-list/component/RegisterForm/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/common/component/Button";
 import SupportingText from "@/common/component/SupportingText";
+import { useToast } from "@/common/hook/useToast";
 import { Form } from "@/shared/component/Form";
 import DateFormController from "@/view/group/problem-list/component/RegisterForm/DateFormController";
 import LinkFormController from "@/view/group/problem-list/component/RegisterForm/LinkFormController";
@@ -19,13 +20,15 @@ type RegisterFormProps = {
   variant?: "default" | "secondary";
 };
 const RegisterForm = ({ variant = "default" }: RegisterFormProps) => {
+  const { showToast } = useToast();
   const form = useForm<z.infer<typeof registerProblemSchema>>({
     resolver: zodResolver(registerProblemSchema),
-    mode: "onTouched",
+    mode: "onSubmit",
   });
 
   const handleSubmit = (values: z.infer<typeof registerProblemSchema>) => {
     console.log({ values });
+    showToast("문제가 정상적으로 등록되었어요.", "success");
   };
   const title = variant === "default" ? "문제 등록하기" : "문제 수정하기";
   return (
