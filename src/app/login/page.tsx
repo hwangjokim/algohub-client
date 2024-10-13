cfc0eba8afb4b86d4a6f7b2dd2be525f165e4831
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-13 19:54:26 +0900

    [ Feat ] Login page 구현 (#93)
    * feat: 로그인 페이지 구현, 유효성 검사 적용
    
    * fix: format error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import Modal from "@/common/component/Modal";
+import ToastProvider from "@/common/component/Toast";
+import AuthHeader from "@/view/index/component/AuthHeader";
+import Card from "@/view/index/component/Card";
+import Footer from "@/view/index/component/Footer";
+import LoginForm from "@/view/login/component/LoginForm";
+import { containerStyle, headingStyle } from "@/view/login/component/index.css";
+import { useRouter } from "next/navigation";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const router = useRouter();
+  const onClose = () => {
+    router.push("/");
+  };
+  return (
+    <Modal isOpen={true} onClose={() => {}}>
+      <AuthHeader handleClick={onClose} />
+      <div className={containerStyle}>
+        <h1 className={headingStyle}>알고허브로 로그인</h1>
+        <Card>
+          <LoginForm />
+          <Footer
+            guideLabel="아직 계정이 없으신가요?"
+            link={{ href: "/signup", label: "회원가입하기" }}
+          />
+        </Card>
+      </div>
+      <ToastProvider />
+    </Modal>
+  );
 };
 
 export default LoginPage;
