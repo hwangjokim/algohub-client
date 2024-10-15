c401da8b0de7edd717c9469752fe0055d4fbe41a
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-16 00:11:52 +0900

    test: branch 잘 있니

---
+++ src/view/user/index/GroupCard/index.css.ts
@@ -0,0 +1,78 @@
+import { theme } from "@/styles/themes.css";
+import { style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+
+export const groupCardWrapper = recipe({
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    padding: "2rem 1.6rem",
+
+    width: "100%",
+
+    borderRadius: "8px",
+    background: theme.color.mg5,
+
+    cursor: "pointer",
+  },
+
+  variants: {
+    isDone: {
+      true: {
+        opacity: 0.2,
+      },
+    },
+  },
+});
+
+export const flexCenterStyle = style({
+  display: "flex",
+  alignSelf: "flex-start",
+
+  alignItems: "center",
+  gap: "0.4rem",
+});
+
+export const imgStyle = style({
+  width: "100%",
+  height: "15.6rem",
+
+  borderRadius: "4px",
+  objectFit: "cover",
+});
+
+export const nameWrapper = style([
+  flexCenterStyle,
+  {
+    paddingTop: "2rem",
+  },
+]);
+
+export const nameStyle = style({
+  color: theme.color.white,
+  ...theme.font.Title1_SB_16,
+});
+
+export const descStyle = style({
+  color: theme.color.mg2,
+  ...theme.font.Caption3_M_12,
+});
+
+export const dateStyle = style([
+  flexCenterStyle,
+  descStyle,
+  {
+    paddingTop: "1rem",
+  },
+]);
+
+export const ownerStyle = style([
+  flexCenterStyle,
+  descStyle,
+  {
+    gap: "0.9rem",
+    padding: "0.7rem 0 0 0.5rem",
+  },
+]);
---
+++ src/view/user/index/GroupCard/index.tsx
@@ -0,0 +1,50 @@
+import type { GroupListItem } from "@/api/group/type";
+import defaultImg from "@/asset/img/alogohub_icon.png";
+import { IcnCalenderCard, IcnUser, IcnUser2 } from "@/asset/svg";
+// import StatusIcon from "@/view/user/index/GroupCard/StatusIcon";
+import {
+  dateStyle,
+  groupCardWrapper,
+  imgStyle,
+  nameStyle,
+  nameWrapper,
+  ownerStyle,
+} from "@/view/user/index/GroupCard/index.css";
+import type { GroupStatus } from "@/view/user/index/type";
+import Image from "next/image";
+import type { HTMLAttributes } from "react";
+
+interface GroupCardProps extends HTMLAttributes<HTMLDivElement> {
+  item: GroupListItem;
+  status: GroupStatus;
+}
+const GroupCard = ({ item, status, ...divProps }: GroupCardProps) => {
+  const { name, groupImage, startDate, endDate, ownerNickname } = item;
+  const isDone = status === "Done";
+
+  return (
+    <article className={groupCardWrapper({ isDone })} {...divProps}>
+      <Image src={groupImage || defaultImg} alt={""} className={imgStyle} />
+      <div className={nameWrapper}>
+        <StatusIcon status={status} />
+        <h1 className={nameStyle}>{name}</h1>
+      </div>
+      <div className={dateStyle}>
+        <IcnCalenderCard width={20} height={20} />
+        <p>{startDate}</p>
+        <p>~</p>
+        <p>{endDate}</p>
+      </div>
+      <div className={ownerStyle}>
+        {isDone ? (
+          <IcnUser2 width={10} height={14.5} />
+        ) : (
+          <IcnUser width={10} height={14.5} />
+        )}
+        <p>{ownerNickname}</p>
+      </div>
+    </article>
+  );
+};
+
+export default GroupCard;
