6d5589aedd73d71b298581c896a887cec50607d7
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-16 10:19:33 +0900

    feat: toast 추가

--- src/view/user/setting/AccountManagementForm/useAccountForm.ts
+++ src/view/user/setting/AccountManagementForm/useAccountForm.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@/common/hook/useToast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import type { z } from "zod";
@@ -13,8 +14,11 @@ const useAccountForm = () => {
       confirmPassword: "1234^aaa",
     },
   });
+  const { showToast } = useToast();
 
-  const handleSubmit = (_values: z.infer<typeof AccountManagementSchema>) => {};
+  const handleSubmit = (_values: z.infer<typeof AccountManagementSchema>) => {
+    showToast("정상적으로 수정이 되었어요", "success");
+  };
 
   const isActive = form.formState.isDirty && form.formState.isValid;
 
