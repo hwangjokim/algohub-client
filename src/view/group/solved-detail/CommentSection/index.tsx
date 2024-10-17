7ecd99390800590d9ed9519c210186ced59bed46
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-18 05:13:09 +0900

    feat: SolvedDetail 레이아웃

---
+++ src/view/group/solved-detail/CommentSection/index.tsx
@@ -0,0 +1,7 @@
+import { sectionWrapper } from "@/view/group/solved-detail/index.css";
+
+const CommentSection = () => {
+  return <section className={sectionWrapper}>CommentSection</section>;
+};
+
+export default CommentSection;
