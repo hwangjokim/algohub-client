c4d481d6d58ef442d37483b3b97889cc8b2c329b
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-10-17 12:47:22 +0900

    feat: 접근성 적용

--- src/view/user/setting/StudyList/SortIcon/index.tsx
+++ src/view/user/setting/StudyList/SortIcon/index.tsx
@@ -1,4 +1,5 @@
 import { IcnBtnArrowDown, IcnBtnSort } from "@/asset/svg";
+import { handleA11yClick } from "@/common/util/dom";
 import { ascIconStyle, sortIconStyle } from "./index.css";
 
 type SortIconProps = {
@@ -11,28 +12,37 @@ const SortIcon = ({ direction: isActive, onClick }: SortIconProps) => {
     case undefined:
       return (
         <IcnBtnSort
+          aria-label="북마크 기준으로 오름차순 정렬하기"
           className={sortIconStyle}
           width={20}
           height={20}
           onClick={onClick}
+          onKeyDown={handleA11yClick(onClick)}
+          tabIndex={0}
         />
       );
     case "asc":
       return (
         <IcnBtnArrowDown
+          aria-label="북마크 기준으로 내림차순 정렬하기"
           className={ascIconStyle}
           width={20}
           height={20}
           onClick={onClick}
+          onKeyDown={handleA11yClick(onClick)}
+          tabIndex={0}
         />
       );
     case "desc":
       return (
         <IcnBtnArrowDown
+          aria-label="북마크 기준 정렬 해제하기"
           className={sortIconStyle}
           width={20}
           height={20}
           onClick={onClick}
+          onKeyDown={handleA11yClick(onClick)}
+          tabIndex={0}
         />
       );
     default:
