fcb860cf71b92501ec0b22a5efdf00301cb50c61
최주용 <121484561+wuzoo@users.noreply.github.com>
2024-10-18 22:40:32 +0900

    [ Feat ] My Solved 페이지 퍼블리싱 (#115)
    * refactor: Problem type 수정 및 list component  반영
    
    * feat: formattedByte, getTierImage 유틸 함수 생성
    
    * refactor: expired를 기준으로 status 선언
    
    * refactor: includes가 아닌 query param을 제외한 pathname과 비교
    
    * feat: Solution type 생성
    
    * feat: my-solved의 list item 퍼블리싱
    
    * feat: my-solved page header 퍼블리싱
    
    * feat: my-solved page header content, grid fraction 상수 선언
    
    * feat: mySolved page publishing
    
    * chore: ci error resolve
    
    * chore: ui 수정
    
    * chore: import 삭제
    
    * chore: type error
    
    * pull main diff 잡기
    
    * chore: underline 제거, 일부 style 수정
    
    ---------
    
    Co-authored-by: Lee jin <83453646+j-nary@users.noreply.github.com>

--- src/app/group/problem-list/page.tsx
+++ src/app/group/problem-list/page.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "@/common/component/Sidebar";
 import { sidebarWrapper } from "@/styles/shared.css";
-import ProblemSidebar from "@/view/group/index/ProblemSidebar";
 
 const ProblemListPage = () => {
-  const isOwner = true;
   return (
     <main className={sidebarWrapper}>
-      <Sidebar>{isOwner ? <ProblemSidebar /> : <></>}</Sidebar>
+      <Sidebar>SideBar</Sidebar>
       {/* children */}
     </main>
   );
--- src/shared/type/index.ts
+++ src/shared/type/index.ts
@@ -1,13 +1,33 @@
 export type Timeout = ReturnType<typeof setTimeout>;
 
 export interface Problem {
-  id: string | number;
-  tier: TierDetail;
   title: string;
-  date: string;
-  solved: number;
-  total: number;
-  status: "solved" | "unsolved" | "wrong";
+  problemId: number;
+  link?: string;
+  startDate: string;
+  endDate: string;
+  level: TierDetail;
+  solved: boolean;
+  submitMemberCount?: number;
+  memberCount?: number;
+  accuracy?: number;
+  inProgress?: boolean;
+}
+
+export interface Solution {
+  solutionId: number;
+  title: string;
+  level: TierDetail;
+  nickname?: string;
+  profileImage?: string;
+  solvedDateTime?: string;
+  content?: string;
+  result?: string;
+  memoryUsage?: number;
+  executionTime?: number;
+  language: LanguageOption;
+  codeLength?: number;
+  commentCount?: number;
 }
 
 export type tier =
@@ -17,5 +37,24 @@ export type tier =
   | "platinum"
   | "diamond"
   | "ruby";
+
+export type LanguageOption =
+  | "C++"
+  | "C99"
+  | "C#"
+  | "C"
+  | "Python"
+  | "node.js"
+  | "Go"
+  | "Java"
+  | "Kotlin";
+
+export type Comment = {
+  commentId: number;
+  writerNickname: string;
+  writerProfileImage: string;
+  content: string;
+  createdAt: string;
+};
 
 export type TierDetail = `${tier} ${1 | 2 | 3 | 4 | 5}` | "master";
