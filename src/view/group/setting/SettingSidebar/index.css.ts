89d02044d29b0cb776c8f43256606e0188f13211
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-18 23:49:13 +0900

    chore: 불필요한 코드 삭제

--- src/view/group/setting/SettingSidebar/index.css.ts
+++ src/view/group/setting/SettingSidebar/index.css.ts
@@ -2,9 +2,6 @@ import { theme } from "@/styles/themes.css";
 import { style } from "@vanilla-extract/css";
 
 export const sidebarWrapper = style({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
   width: "100%",
   padding: "4.2rem 2.4rem",
 });
