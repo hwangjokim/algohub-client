6e9078527c5dea282d5891c2dabc409649cccbbc
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-18 23:28:29 +0900

    feat: GroupSetting Sidebar 퍼블리싱

--- src/view/group/setting/SettingSidebar/index.css.ts
+++ src/view/group/setting/SettingSidebar/index.css.ts
@@ -1,3 +1,4 @@
+import { theme } from "@/styles/themes.css";
 import { style } from "@vanilla-extract/css";
 
 export const sidebarWrapper = style({
@@ -6,4 +7,23 @@ export const sidebarWrapper = style({
   alignItems: "center",
   width: "100%",
   padding: "4.2rem 2.4rem",
+});
+
+export const submitWrapper = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  paddingTop: "2.4rem",
+});
+
+export const editTextStyle = style({
+  color: theme.color.mg2,
+  ...theme.font.Caption3_M_12,
+});
+
+export const deleteTextStyle = style({
+  borderBottom: `1px solid ${theme.color.mg3}`,
+  color: theme.color.mg3,
+  ...theme.font.Caption3_M_12,
 });
--- src/view/group/setting/SettingSidebar/index.tsx
+++ src/view/group/setting/SettingSidebar/index.tsx
@@ -1,10 +1,39 @@
-import EditAvatar from "@/shared/component/EditAvatar";
-import { sidebarWrapper } from "@/view/group/setting/SettingSidebar/index.css";
+"use client";
+import { groupSchema } from "@/shared/api/schema";
+import GroupInfoForm from "@/shared/component/GroupInfoForm";
+import {
+  deleteTextStyle,
+  editTextStyle,
+  sidebarWrapper,
+  submitWrapper,
+} from "@/view/group/setting/SettingSidebar/index.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
 
 const SettingSidebar = () => {
+  const form = useForm<z.infer<typeof groupSchema>>({
+    resolver: zodResolver(groupSchema),
+    mode: "onTouched",
+    defaultValues: {
+      image: "",
+      name: "",
+      desc: "",
+    },
+  });
+
   return (
     <div className={sidebarWrapper}>
-      <EditAvatar />
+      <GroupInfoForm form={form} variant="group-setting">
+        <div className={submitWrapper}>
+          <button type="submit" disabled={!form.formState.isValid}>
+            <p className={editTextStyle}>수정하기</p>
+          </button>
+          <button type="button">
+            <p className={deleteTextStyle}>스터디 삭제하기</p>
+          </button>
+        </div>
+      </GroupInfoForm>
     </div>
   );
 };
