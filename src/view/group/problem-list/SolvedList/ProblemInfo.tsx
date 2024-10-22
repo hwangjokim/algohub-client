d7b14df5a7ab90d561344dc02a82c4256afa63d3
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-23 02:28:49 +0900

    feat: ProblemInfo Header 추가

--- src/view/group/problem-list/SolvedList/ProblemInfo.tsx
+++ src/view/group/problem-list/SolvedList/ProblemInfo.tsx
@@ -3,16 +3,19 @@ import { solvedListStyle } from "@/view/group/solved-detail/index.css";
 
 const ProblemInfo = () => {
   return (
-    <ProblemList.Item
-      problemId={1}
-      title="트리에서의 동적 계획법"
-      startDate="2024-01-01"
-      endDate="2024-01-02"
-      level="bronze 1"
-      solved={true}
-      memberCount={200}
-      className={solvedListStyle}
-    />
+    <ProblemList>
+      <ProblemList.Header />
+      <ProblemList.Item
+        problemId={1}
+        title="트리에서의 동적 계획법"
+        startDate="2024-01-01"
+        endDate="2024-01-02"
+        level="bronze 1"
+        solved={true}
+        memberCount={200}
+        className={solvedListStyle}
+      />
+    </ProblemList>
   );
 };
 
