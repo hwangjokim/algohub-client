5ab45767ff643a3c81ad5d8edb4e562d79018d1e
최주용 <121484561+wuzoo@users.noreply.github.com>
2024-11-27 23:04:50 +0900

    fix: aws env 임시 토큰으로 대체 (#227)

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,13 +11,15 @@ export const kyInstance = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_HOST,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3MzI4MDI1NDR9.5oI0_Ja7nSxuFgor5WwDDoKOt2-MEMvGp63M7YRD2gs",
   },
 });
 
 export const kyFileInstance = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_HOST,
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3MzI4MDI1NDR9.5oI0_Ja7nSxuFgor5WwDDoKOt2-MEMvGp63M7YRD2gs",
   },
 });
